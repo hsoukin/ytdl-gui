9e132f9f135e4c00630223b00675e79557eee9fe
hsoukin <domb.g@libero.it>
2020-07-17 16:57:27 +0200

    Initial Commit

---
+++ gui.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+
+import os
+import PySimpleGUIQt as sg
+import ytdl_script as dl
+
+
+sg.theme('Light Green 2')  # please make your creations colorful
+
+url = 'url-input'
+folder = 'folder-input'
+btn_audio = 'audio-input'
+
+layout = [
+            [sg.Text('Link to video or playlist'), ],
+            [sg.Input(focus=True, key=url), ],
+            [sg.Text('Output Folder'), ],
+            [sg.Input(key=folder), ],
+            [
+                sg.FolderBrowse(target=folder),
+                sg.Radio('Audio Only', btn_audio, key='audio', default=True), sg.Radio('Video', btn_audio, key='video'),
+                sg.Checkbox('Suppress Errors', key='no-error', default=False),
+            ],
+            [sg.Button('Download'), sg.CloseButton('Exit')]
+         ]
+
+window = sg.Window('Youtube Downloader GUI', layout,
+                   return_keyboard_events=True, use_default_focus=False)
+
+while True:
+    event, values = window.read(timeout=200)
+    if event in (sg.WIN_CLOSED, 'Exit'):
+        break
+    elif event in ('Enter', 'Download', 'Return::35'):
+        print("Hit OK")
+        og_dir = os.getcwd()
+        try:
+            os.chdir(values[folder])
+            args = [f'{values[url].strip()}']
+            downloaded = '\n'.join(dl.download(args, audio=values['audio'], ignore_errors=values['no-error']))
+            sg.popup('Media downloaded successfully as:\n'
+                     f'{downloaded}.\n', f'Output Location: {os.getcwd()}',
+                     title='Success', custom_text='OK')
+        except Exception as e:
+            print(e)
+            if not values['no-error']:
+                sg.popup('Something went wrong while\ndownloading the provided media.',
+                         title='Error', custom_text='Cancel')
+
+        os.chdir(og_dir)
+
+
+window.close()
---
+++ ytdl_script.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python
+
+from __future__ import unicode_literals
+import sys
+import youtube_dl
+
+
+class LoggerClass(object):
+    def debug(self, msg):
+        print('DEBUG:', msg)
+    
+    def warning(self, msg):
+        print('WARNING:', msg)
+    
+    def error(self, msg):
+        print('ERROR:', msg)
+
+
+def print_status(output_list=[]):
+    def status_foo(dl):
+        if dl["status"] == "finished":
+            name = dl['filename']
+            print(f"{name} was downloaded successfully.")
+            output_list.append(str(name))
+
+    return status_foo
+
+
+def download(args, audio=True, ignore_errors=False):
+    downloaded = []
+    down_format = 'm4a' if audio else 'mp4'
+
+    down_opts = {
+        "format": down_format,
+        "merge_output_format": down_format,
+        "outtmpl": "%(title)s.%(ext)s",
+        "writethumbnail": True,
+        "ignoreerrors": ignore_errors,
+        # "restrictfilenames": True,
+        # "download_archive": f"download-list.txt",
+        "postprocessors": [
+            {"key": "FFmpegMetadata"},
+        ],
+        "progress_hooks": [print_status(output_list=downloaded)],
+        "logger": LoggerClass(),
+    }
+
+    audio_opts = [
+        {"key": "EmbedThumbnail"},
+        {
+            "key": "FFmpegExtractAudio",
+            "preferredcodec": 'm4a',
+        },
+    ]
+
+    if audio:
+        down_opts['postprocessors'].extend(audio_opts)
+
+    try:
+        with youtube_dl.YoutubeDL(down_opts) as ydl:
+            print(f'Downloading {args}...')
+            ydl.download(args)
+            print(downloaded)
+            return downloaded
+    except Exception as e:
+        if __name__ != '__main__' and str(e) != '\'mp4\'':
+            raise
+            print(e)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        arg_list = [arg for arg in sys.argv[1:]]
+    else:
+        print("Please paste in the URL to the videos you'd like to",
+              "download, separated by spaces.")
+        arg_list = input("").split(" ")
+    download(arg_list)
+    sys.exit(0)
